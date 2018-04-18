2f490176dddc46854a4919d8e53e592dc76170da
Alexandru Dan <dan.alex97@yahoo.com>
2018-04-18 15:52:55 +0100

    Tested seam.

--- cache_torrent/tracker.go
+++ cache_torrent/tracker.go
@@ -14,3 +14,15 @@ func (t *Tracker) New(util TorrentNodeUtil) TorrentNode {
   tracker.Tracker = (tracker.Tracker.New(util)).(*torrent.Tracker)
   return tracker
 }
+
+func (t *Tracker) OnJoin() {
+  go t.CheckMessages(t.Recv)
+}
+
+func (t *Tracker) Recv(m interface {}) {
+  switch m.(type) {
+  default:
+    /* Backward compatibility. */
+    t.Tracker.Recv(m)
+  }
+}
