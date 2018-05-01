ee7e93edb78e946dc28e2c31c7c022512c74a9f9
Alexandru Dan <dan.alex97@yahoo.com>
2018-05-01 17:29:37 +0100

    Added profiling even on signals.

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
   errLog "log"
   "runtime/pprof"
   "runtime"
+  "os/signal"
+  "os"
 
   "flag"
   "math/rand"
   "time"
   "sync"
   "fmt"
-  "os"
 )
 
 // Flags
@@ -45,6 +46,21 @@ var verbose = flag.Bool(
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
 var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
+func makeMemprofile() {
+  // Profiling
+  if *memprofile != "" {
+    f, err := os.Create(*memprofile)
+    if err != nil {
+        errLog.Fatal("could not create memory profile: ", err)
+    }
+    runtime.GC() // get up-to-date statistics
+    if err := pprof.WriteHeapProfile(f); err != nil {
+        errLog.Fatal("could not write memory profile: ", err)
+    }
+    f.Close()
+  }
+}
+
 func main() {
   // Parsing the flags
   flag.Parse()
@@ -60,6 +76,20 @@ func main() {
     }
     defer pprof.StopCPUProfile()
   }
+  defer makeMemprofile()
+  // Get profile even on signal
+  c := make(chan os.Signal, 1)
+  signal.Notify(c, os.Interrupt)
+  go func(){
+    for sig := range c {
+      fmt.Println("Singal received:", sig)
+      if *cpuprofile != "" {
+        pprof.StopCPUProfile()
+      }
+      makeMemprofile()
+      os.Exit(0)
+    }
+  }()
 
   // Random seed
   rand.Seed(time.Now().UTC().UnixNano())
@@ -115,17 +145,4 @@ func main() {
   log.Query(log.GetTraffic)
   log.Query(log.GetTime)
   log.Query(log.Stop)
-
-  // Profiling
-  if *memprofile != "" {
-    f, err := os.Create(*memprofile)
-    if err != nil {
-        errLog.Fatal("could not create memory profile: ", err)
-    }
-    runtime.GC() // get up-to-date statistics
-    if err := pprof.WriteHeapProfile(f); err != nil {
-        errLog.Fatal("could not write memory profile: ", err)
-    }
-    f.Close()
-  }
 }
