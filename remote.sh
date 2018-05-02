41256d64b341450cf8e73e86daa3a9e38668b219
Alexandru Dan <dan.alex97@yahoo.com>
2018-05-02 07:37:19 +0100

    Remote can run detached on network.

--- remote.sh
+++ remote.sh
@@ -1,12 +1,3 @@
 ARGS=$@
-ssh ad5915@shell3.doc.ic.ac.uk << ENDSSH
-  echo "Connected to shell."
-  echo "Job: $ARGS"
-  ssh -f -o StrictHostKeyChecking=no ad5915@matrix01.doc.ic.ac.uk "
-    echo 'Dispaching: $ARGS'
-    cd ~/golang/src/github.com/danalex97/nfsTorrent
-    nohup python3 remote.py $ARGS > /dev/null 2>&1 &
-    echo 'Job dispached.'
-    exit
-  " &
-ENDSSH &
+echo "Running job: $ARGS"
+nohup python3 reomte.py $ARGS > /dev/null 2>&1 &
