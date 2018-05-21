c028d5dbfab212e30b3c0b7f1f31f88927da01d0
Alexandru Dan <dan.alex97@yahoo.com>
2018-05-21 12:54:44 +0100

    Added startup functions to context.

--- visual/js/draw.js
+++ visual/js/draw.js
@@ -1,4 +1,4 @@
-Drawer = function(ctx, nodes) {
+NodeDrawer = function(ctx, nodes) {
   let self = this;
 
   let center = ctx.svg
@@ -6,27 +6,46 @@ Drawer = function(ctx, nodes) {
     .attr("transform", "translate(" +
       ctx.width / 2 + "," +
       ctx.height / 2 + ")");
-  self.node  = center
-    .append("g")
-    .selectAll(".node");
-  self.nodes = nodes;
 
-  self.tick = function() {
-    self.node
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
+  function draw(ctx) {
+    return ctx
+      .append("circle")
+      .attr("fill", "red")
+      .attr("r", 8);
   }
 
-  self.restart = function() {
+  function restart() {
     // Apply the general update pattern to the nodes.
     self.node = node.data(self.nodes);
     self.node.exit().remove();
-    self.node = self.node.enter().append("circle").attr("fill", "red").attr("r", 8).merge(self.node);
+    self.node = draw(self.node.enter()).merge(self.node);
 
     // Update and restart the simulation.
     ctx.simulation.nodes(self.nodes);
     ctx.simulation.alpha(1).restart();
   }
+
+  /* Fields. */
+  self.node  = center
+    .append("g")
+    .selectAll(".node");
+  self.nodes = nodes;
+
+  /* Interface. */
+  self.tick = function() {
+    self.node
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+  };
+
+  self.addNode = function(node) {
+    self.nodes.push(Node());
+    restart();
+  };
+
+  self.start = function() {
+    restart();
+  };
 
   return self;
 }
--- visual/js/simulation.js
+++ visual/js/simulation.js
@@ -7,18 +7,36 @@ Ctx = function() {
     });
   }
 
+  function start() {
+    self.starters.forEach(function(starter) {
+      starter.start();
+    });
+  }
+
+  /* Fields. */
   self.svg    = d3.select("svg");
   self.width  = +self.svg.attr("width");
   self.height = +self.svg.attr("height");
+
   self.simulation = d3.forceSimulation()
       .force("charge", d3.forceManyBody())
       .on("tick", tick);
 
-  self.tickers = [];
+  self.tickers  = [];
+  self.starters = [];
 
+  /* Interface. */
   self.addTicker = function(ticker) {
     self.tickers.push(ticker);
-  }
+  };
+
+  self.addStarter = function(starter) {
+    self.starters.push(starter);
+  };
+
+  self.start = function() {
+    start();
+  };
 
   return self
 }
