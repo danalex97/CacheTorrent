88046d88d20d5eb03ea1f056cf904b04fedb7845
Alexandru Dan <dan.alex97@yahoo.com>
2018-05-21 10:20:09 +0100

    Added nodes update.

--- visual/js/draw.js
+++ visual/js/draw.js
@@ -7,8 +7,7 @@ const svg = d3.select('svg')
 
 const simulation = d3.forceSimulation()
   .force('charge', d3.forceManyBody().strength(-20))
-  .force('center', d3.forceCenter(width / 2, height / 2))
-
+  .force('center', d3.forceCenter(width / 2, height / 2));
 
 let nodes = [
   Node(),
@@ -18,15 +17,13 @@ let nodes = [
   Node()
 ]
 
-let nodes_data = nodes;
-
-console.log(nodes_data)
-simulation.nodes(nodes_data);
+console.log(nodes)
+simulation.nodes(nodes);
 
 var node = svg.append("g")
   .attr("class", "nodes")
   .selectAll("circle")
-  .data(nodes_data)
+  .data(nodes)
   .enter()
   .append("circle")
   .attr("r", 5)
@@ -40,6 +37,37 @@ function tickActions() {
     .attr("cy", function(d) {
       return d.y;
     })
+}
+
+d3.timeout(function() {
+  nodes.push(Node());
+  restart();
+}, 1000);
+
+d3.timeout(function() {
+  nodes.push(Node());
+  restart();
+}, 2000);
+
+function restart() {
+  // Apply the general update pattern to the nodes.
+  node = node.data(nodes, function(d) {
+    return d.id;
+  });
+  node.exit().remove();
+  node = node
+    .enter()
+    .append("circle")
+    .attr("fill", "red")
+    .attr("r", 5)
+    .merge(node);
+
+  // Update and restart the simulation.
+  simulation
+    .nodes(nodes);
+  simulation
+    .alpha(1)
+    .restart();
 }
 
 simulation.on("tick", tickActions);
