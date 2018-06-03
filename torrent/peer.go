e8dcbab9cd995977f0fc4e7b3e999122c66d5fdf
Alexandru Dan <dan.alex97@yahoo.com>
2018-06-03 23:59:09 +0100

    Commented the torrent/peer file.

--- torrent/peer.go
+++ torrent/peer.go
@@ -16,20 +16,25 @@ var progress config.Const = config.NewConst(config.AllNodesRun)
 type Peer struct {
   *Components
 
+  // Information used for initializing the peer.
   Id      string
   Tracker string
   Ids     []string
 
-  // BitTorrent protocol
+  // -- BitTorrent protocol --
   Pieces []PieceMeta
 
-  // BitTorrent components
-  Connectors  map[string]Runner // the connectors that were chosen by tracker
+  // -- BitTorrent components --
+  // A map of Connectiors for all the nodes provided by the Tracker or by other
+  // peers via connection initialization.
+  Connectors  map[string]Runner
 
-  // used only to identify tracker
-  join    string
+  // Used only to identify tracker via connecting a bootstrap node which
+  // follows the BitTorrent protocol.
+  join string
 
-  // used to keep if logging is on
+  // Used to keep if logging is on. Logs can be visualized via the vizualtion
+  // tool in 'visual' folder.
   logging bool
 }
 
@@ -37,6 +42,18 @@ type Binder    func(m interface {}) int
 type Processor func(m interface {}, state int)
 type ConnAdder func(id string)
 
+// The Bind states of a peer are:
+//  - BindNone - the initial state during the bind stage; if the message format
+//     is unexpected the state will remain BindNode
+//  - BindDone - the state which announced that work was done, even though the
+//     work is done only for building the peer's connections
+//  - BindRecv - if the initialization phase is done(i.e. the peer began to
+//     build its connection list) and the message can be passed towards a
+//     specific Connection based on the sender's ID
+//  - BindRun - the BindRun state anounced that the phase Tracker sent all the
+//     necessary informations so we can finish the initialization phase; when
+//     the peer arrives in the BindRun state the function Run is called
+//     synchronously
 const (
   BindNone = iota
   BindDone = iota
@@ -44,7 +61,8 @@ const (
   BindRun  = iota
 )
 
-/* Implementation of Torrent Node interface. */
+// The OnJoin function is called when the peer join the system and is part of
+// the Implementation of Torrent Node interface.
 func (p *Peer) OnJoin() {
   // If the Transport is missing, it must be we are
   // on torrent-less node
@@ -59,14 +77,20 @@ func (p *Peer) OnJoin() {
   }()
 }
 
+// The OnLeave function is called when the peer leaves0 the system and is
+// part of the Implementation of Torrent Node interface.
 func (p *Peer) OnLeave() {
 }
 
+// The New function is the construction function for the TorrentNode. The
+// peer should be partially initialized in this function by using the
+// TorrentNodeUtil's components. The TorrentNodeUtil is an interface which
+// allows a node to interact with the Speer simulator.
 func (p *Peer) New(util TorrentNodeUtil) TorrentNode {
   peer := new(Peer)
 
-  peer.Id        = util.Id()
-  peer.join      = util.Join()
+  peer.Id         = util.Id()
+  peer.join       = util.Join()
   peer.Components = new(Components)
   peer.Transport  = util.Transport()
 
@@ -78,9 +102,12 @@ func (p *Peer) New(util TorrentNodeUtil) TorrentNode {
   return peer
 }
 
-/* Internal functions. */
+// Initilization function called synchronously after a node has joined the
+// peer-to-peer system. In this function we find who the tracker is by
+// interacting with the bootstrap node, respond to other peers which ask
+// who the tracker is and, finally, send the tracker a Join request.
 func (p *Peer) Init() {
-  // Find out who the tracker is
+  // Find out who the tracker is.
   p.Transport.ControlSend(p.join, TrackerReq{p.Id})
 
   queue    := []TrackerReq{}
@@ -112,6 +139,14 @@ func (p *Peer) Init() {
   p.logging = log.HasLogfile();
 }
 
+// CheckMessages is a function which runs until the node departs the system.
+// It registers all the incoming messages. The messages are checked using a
+// Binder which identifies the state of the Peer and the messages are processed
+// by being passed to a Processor. The Processor looks at the state returned by
+// the binder and acts correspondingly dispaching the message accordingly.
+//
+// The CheckMessages function is exposed in the public API to make building on
+// the current protocol easier by following the same Binder-Processor pattern.
 func (p *Peer) CheckMessages(bind Binder, process Processor) {
   for {
     messages := []interface{}{}
@@ -171,6 +206,9 @@ func (p *Peer) CheckMessages(bind Binder, process Processor) {
   }
 }
 
+// Process is a Processor function which calls the Run function when the node
+// reaches the BindRun state. Afterwards, when the node is in the BindRecv
+// state, the Process function will redirect the message to the RunRecv method.
 func (p *Peer) Process(m interface {}, state int) {
   switch state {
   case BindRun:
@@ -216,21 +254,25 @@ func (p *Peer) Bind(m interface {}) (state int) {
   return
 }
 
+// The run function is called at the end of the initialization phase. It
+// initializes most of the internal components of a peer exposed by the
+// Components structure. Moreover, it starts all the asynchronous routines such
+// as the Choker and registers all the connections provided by the Tracker.
 func (p *Peer) Run(connAdd ConnAdder) {
   // We want to bind all these variables here, so
   // we don't need any synchroization.
   // log.Println(p.Id, p.Ids)
 
-  // make per peer variables
+  // Make per peer components.
   p.Storage   = NewStorage(p.Id, p.Pieces, p.Time)
   p.Picker    = NewPicker(p.Storage)
   p.Transport = p.Transport
   p.Manager   = NewConnectionManager()
   p.Choker    = NewChoker(p.Manager, p.Time)
 
-  // Make connectors
+  // Make connectors.
   for _, id := range p.Ids {
-    // Defensive programming
+    // Defensive programming.
     if _, ok := p.Connectors[id]; !ok {
       connAdd(id)
     }
@@ -239,6 +281,7 @@ func (p *Peer) Run(connAdd ConnAdder) {
   go p.Choker.Run()
 }
 
+// A function used to extract the Id from an incoming message.
 func (p *Peer) GetId(m interface {}) (id string) {
   switch msg := m.(type) {
   case Choke:
@@ -261,30 +304,28 @@ func (p *Peer) GetId(m interface {}) (id string) {
   return
 }
 
+// The RunRecv function is used to dispach the message towards a
+// Connector's Recv method.
 func (p *Peer) RunRecv(id string, m interface {}, connAdd ConnAdder) {
   if id == "" {
     return
   }
 
-  /**
-   * The peer can have up to at most config.Config.OutPeers connections
-   * initiated by it.
-   *
-   * A perfect tracker would not keep "one-way" connections, that
-   * is connections that are not reciprocated. (i.e. accepted as an
-   * in-connection)
-   *
-   * To handle this, some protocols allow the peers to be
-   * periodically changed. However, when using a perfect tracker, this
-   * is not needed and we can set OutPeers = 0.
-   */
+  // The peer can have up to at most config.Config.OutPeers connections
+  // initiated by it.
+  //
+  // A perfect tracker would not keep "one-way" connections, that
+  // is connections that are not reciprocated. (i.e. accepted as an
+  // in-connection)
+  //
+  // To handle this, some protocols allow the peers to be
+  // periodically changed. However, when using a perfect tracker, this
+  // is not needed and we can set OutPeers = 0.
 
   // if _, ok := p.Connectors[id]; !ok && len(p.Connectors) < inPeers + outPeers {
   if _, ok := p.Connectors[id]; !ok {
-    /*
-     * This should not be reached when we having a perfect tracker.
-     */
-     connAdd(id)
+    // This should not be reached when we having a perfect tracker.
+    connAdd(id)
   }
 
   if connector, ok := p.Connectors[id]; ok {
@@ -292,6 +333,8 @@ func (p *Peer) RunRecv(id string, m interface {}, connAdd ConnAdder) {
   }
 }
 
+// Function called when a new Connector towards the node id should be
+// initialized.
 func (p *Peer) AddConnector(id string) {
   NewConnector(p.Id, id, p.Components).
   WithUpload(NewUpload).
