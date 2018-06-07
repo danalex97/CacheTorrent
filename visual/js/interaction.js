00b6eb10599914b2ffafc1e467ae240cd48002a1
Alexandru Dan <dan.alex97@yahoo.com>
2018-06-07 19:43:26 +0100

    Finished visualization's back-end.

--- visual/js/interaction.js
+++ visual/js/interaction.js
@@ -4,8 +4,30 @@ function format(res) {
   return res;
 }
 
+// Radio button state.
+let currentValue = 1;
+function handleClick(myRadio) {
+  currentValue = myRadio.value;
+
+  $('#option1').removeClass('active');
+  $('#option2').removeClass('active');
+
+  $('#option' + currentValue).addClass('active');
+}
+
+function amRequesting() {
+  return currentValue == 2;
+}
+
+// Request state.
 function request(data) {
   let where = data.log;
+
+  if (!amRequesting()) {
+    get_log(where, main);
+    return;
+  }
+
   $.ajax({
     method: "POST",
     url: "http://127.0.0.1:8080/job",
